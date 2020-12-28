15a880b2db31ec7aaf991213142885ba9f2cf579
Chad Lengefeld <chadlengefeld@gmail.com>
2020-12-28 12:30:32 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -2,7 +2,7 @@ class SongsController < ApplicationController
   def index
     if params[:artist_id]
 binding.pry
-      @songs = Artist.find_by(params[:artist_id].songs)
+      @songs = Artist.find_by(params[:artist_name].songs)
 
     else
     @songs = Song.all
