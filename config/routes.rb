45d3678f3c46dfa4a68bf3e87a146d8029ee3ac4
Chad Lengefeld <chadlengefeld@gmail.com>
2020-12-28 12:24:15 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  resources :artists
-  resources :songs
+  resources :artists, only: [:show] do
+  resources :songs, only: [:index, :show, :new, :create, :edit, :update]
+  end
 
   get 'artists/:id/songs', to: 'artist#songs_index'
   get 'artist/:id/songs/:song_id', to: 'artists#show'
 
   root 'songs#index'
-  
+
 end
