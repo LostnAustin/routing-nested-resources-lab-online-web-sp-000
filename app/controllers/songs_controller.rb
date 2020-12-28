1e8a1a5b9d9640fdbfe02ec392fc6a62c3738060
Chad Lengefeld <chadlengefeld@gmail.com>
2020-12-28 12:42:19 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -3,7 +3,7 @@ class SongsController < ApplicationController
     if params[:artist_id]
       @artist = Artist.find_by(id: params[:artist_id])
 
-    if !@artist.nil?
+    if @artist.nil?
     else
       @songs = @artist.songs
     end
