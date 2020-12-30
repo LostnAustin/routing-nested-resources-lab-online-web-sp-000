6415f14f84179a6e08da528194c9920a85da7bdc
Chad Lengefeld <chadlengefeld@gmail.com>
2020-12-30 06:50:38 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -22,7 +22,7 @@ class SongsController < ApplicationController
             redirect_to artist_songs_path(@artist), alert: "Song not found"
           end
         else
-          @song = Song.find(id: params[:id])
+          @song = Song.find(params[:id])
     end
   end
 
